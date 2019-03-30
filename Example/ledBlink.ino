017b10c289813e192b501d168d5ff7acae59e884
DC Back <45322614+dcback@users.noreply.github.com>
2019-03-30 13:33:36 +0900

    Create ledBlink.ino

---
+++ Example/ledBlink.ino
@@ -0,0 +1,18 @@
+#define ledPin  2   // 2 == GPIO2
+unsigned int cnt;
+ 
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+    
+    Serial.println(cnt);
+    cnt++;
+}
