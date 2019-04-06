24bc43d2987bcfa0f16c9df2d2097dbe46d053b6
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-06 14:00:49 +0900

    Update led_Blink.ino

--- Example/Display/led_Blink.ino
+++ Example/Display/led_Blink.ino
@@ -1,1 +1,18 @@
-aa
+#define ledPin  2   // == D2
+unsigned int cnt;
+ 
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+    
+    Serial.println(cnt);
+    cnt++;
+}
