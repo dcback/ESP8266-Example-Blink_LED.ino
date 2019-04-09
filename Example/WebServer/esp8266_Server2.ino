292729ab586da3cfc0d0e08668ba2e6b5747fc10
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-09 21:08:29 +0900

    Update esp8266_Server2.ino

--- Example/WebServer/esp8266_Server2.ino
+++ Example/WebServer/esp8266_Server2.ino
@@ -17,11 +17,21 @@ const char htmlPage[] PROGMEM = R"=====(
 
 <body>
     <div class="container">
-        <h1>ESP8266 HTTP Server</h1>
+        <h1>ESP8266 KiET HTTP Server</h1>
         <p>Hello from ESP8266 HTTP Server. Selection of <b>Bootstrap</b> CSS styles.</p>
-        <h2>H2</h2>
-        <h3>H3</h3>
-        <h4>H4</h4>
+        <h2>H2 KiET Sever designed by DC Back</h2>
+        <h3>H3 무한 봉사 무한 행복 무한 생명</h3>
+        <h4>H4 사후가 있건 없건 내 내리사랑과 봉사만이 우리의 존재의 이유이다. 이는 세상의 모든 종교의 
+        원리이며 가르침이다, 우리 곁에 진정한 신은 어렵고 힘든 이웃이다, 물질이든 시간이든 정신이든 
+        지식이든 내 가진 모든 자산을 헌납하고 삶의 종착지에선 조용히 웃으며 홀연히 떠나는 것이다</h4>
+        <h5>H5 물은 인간이다.. 안개를 이루는 미세 물 입자는 습을 유지해주고 자연을 살 찌우고 구름을 
+        만들어 비를 내리고 흐르는 강물이 되어 바다로 바다로 흘러... 다시 하늘로 승천하여 새로운 물 입자로 
+        세상을 건강하게 유지하는데 봉사한다. 비단 물 뿐만 아니라 내 모든 구성요소 아니 모든 자연의 생물, 
+        무생물이 모두 이렇게 환원한다. 내 현재 몸은 이들의 결합이고 너무나도 운좋게 정신과 자아를 가졌기에 
+        세상에 모든 구성원에게 봉사와 희생은 지극히 당연한 것이다. 그런데 단지 인간만이 철저히 자기를 위해서만 산다</h5>
+        <h6>H6 아낌없이 주는 나무가 있습니다.열매를, 시원한 그늘을, 나중엔 잘려나가,그루터기 의자로 남아 지친 
+        이의 쉼터가 되어주는.. 어떤 나무도 자신을 위해 자라지 않습니다. 자신을 위해 그늘을 만들지 않습니다</h6>
+        
         <h1>Buttons</h1>
         <p>
             <button type="button" class="btn btn-default">Default</button>
@@ -47,7 +57,7 @@ const char htmlPage[] PROGMEM = R"=====(
             <strong>Oh snap!</strong> Change a few things up and try submitting again.
         </div>
 
-        <p>Copyright (C) 2019 <a href="https://www.arduinoslovakia.eu">Arduino Slovakia</a>.</p>
+        <p>Copyed by DC Back (C) 2019 <a href="https://github.com/dcback/">DC Back Arduino GitHub </a>.</p>
     </div>
 </body>
 
@@ -63,10 +73,17 @@ void handleRoot() {
 const char* ssid     = "Backhomenet";
 const char* password = "1700note";
 
-void setup(void){
+unsigned long preMillis = 0;
+#define ledPin  D4
+int ledState = LOW;
+
+void setup(){
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   Serial.println("");
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -83,8 +100,19 @@ void setup(void){
 
   server.begin();
   Serial.println("HTTP server started");
+  digitalWrite(ledPin, LOW);
 }
 
-void loop(void){
+void loop(){
   server.handleClient();
+
+  unsigned long curMillis = millis();
+
+  if ( curMillis - preMillis >= 1000 ) {  // delay(1000);
+      preMillis = curMillis;
+      if (ledState == LOW) ledState = HIGH;
+      else ledState = LOW;
+      digitalWrite(ledPin, ledState);
+      Serial.print(ledState);
+  }
 }
