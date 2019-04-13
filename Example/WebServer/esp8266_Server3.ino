37d7edf421c4610c23fd01c920cac2e5a8d9ed4f
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-13 12:54:32 +0900

    Update esp8266_Server3.ino

--- Example/WebServer/esp8266_Server3.ino
+++ Example/WebServer/esp8266_Server3.ino
@@ -14,10 +14,11 @@ int     cnt         = 0;
 ESP8266WebServer server ( 80 );
 
 String getPage() {
-    String page = "<html lang=kr-KR><head><meta http-equiv='refresh' content='10'/>";
+    String page = "<html lang='en'><head><meta http-equiv='refresh' content='10'/>";
     page += "<title>ESP8266 Server Demo</title>";
     page += "<style> body { background-color: #fffff; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>";
     page += "</head><body><h1>ESP8266 Demo</h1>";
+    page += "<h2>Sensing & Control Mini station</h2>";
     page += "<h3>BME280</h3>";
     page += "<ul><li>TEMPERATURE : ";
     page += tempValue;
@@ -32,11 +33,11 @@ String getPage() {
     page += "<form action='/' method='POST'>";
     page += "<ul><li>D4 (State : ";
     page += ledState;
-    page += ")";
+    page += ")  ";
     page += "<INPUT type='radio' name='LED' value='0'>OFF";
     page += "<INPUT type='radio' name='LED' value='1'>ON</li></ul>";
     page += "&nbsp; &nbsp; &nbsp;<INPUT type='submit' value='SUBMIT'>";
-    page += "<p><a hrf='https://diyprojects.io/esp8266-web-server-tutorial-create-html-interface-connected-object/#.XK-nvpgzaiM'><h5>Designed by analoglab.com</h5></p>";
+    page += "<p><a href='https://github.com/dcback/ESP8266/blob/master/Example/WebServer/esp8266_Server3.ino' target='_blank'>GithubLink</p>";
     page += "</body></html>";
     return page;
 }
@@ -100,9 +101,10 @@ void loop() {
     humiValue = 50.0 + cnt;
     co2Value = 1000.0 + cnt;
     cnt++;
-    delay(1000);
+
     if ( cnt == 10 ) cnt = 0;
     Serial.print("T : "); Serial.println(tempValue);
     Serial.print("H : "); Serial.println(humiValue);
     Serial.print("CO2 : "); Serial.println(co2Value);
+    delay(1000);
 }
