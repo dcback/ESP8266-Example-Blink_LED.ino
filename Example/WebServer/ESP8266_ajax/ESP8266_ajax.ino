a6b6101537549a341e1530523e60174b891879d2
DC Back <45322614+dcback@users.noreply.github.com>
2019-04-21 15:50:30 +0900

    Update ESP8266_ajax.ino

--- Example/WebServer/ESP8266_ajax/ESP8266_ajax.ino
+++ Example/WebServer/ESP8266_ajax/ESP8266_ajax.ino
@@ -1,50 +1,89 @@
-// https://circuits4you.com/2018/02/04/esp8266-ajax-update-part-of-web-page-without-refreshing/
-const char MAIN_page[] PROGMEM = R"=====(
-<!DOCTYPE html>
-<html>
-<body>
+/*
+    ESP8266 NodeMCU AJAX Demo
+    Updates and Gets data from webpage without page refresh
+    https://circuits4you.com
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
-<div>
-<h1>The ESP8266 NodeMCU Update web page without refresh</h1>
-    <button type="button" onclick="sendData(1)">LED ON</button>
-    <button type="button" onclick="sendData(0)">LED OFF</button><BR>
-</div>
+#include "index.h" //Our HTML webpage contents with javascripts
 
-<div>
-    ADC Value is : <span id="ADCValue">0</span><br>
-    LED State is : <span id="LEDState">NA</span>
-</div>
-<script>
-function sendData(led) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("LEDState").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "setLED?LEDstate="+led, true);
-  xhttp.send();
+#define LED 2  //On board LED
+
+//SSID and Password of your WiFi router
+const char* ssid = "Backhomenet";
+const char* password = "1700note";
+
+ESP8266WebServer server(80); //Server on port 80
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+    String s = MAIN_page; //Read HTML contents
+    server.send(200, "text/html", s); //Send web page
 }
 
-setInterval(function() {
-  // Call a function repetatively with 1 Second interval
-  getData();
-}, 1000); //2000mSeconds update rate
+void handleADC() {
+    int a = 99999; // analogRead(A0);
+    String adcValue = String(a);
 
-function getData() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("ADCValue").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "readADC", true);
-  xhttp.send();
+    server.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
 }
-</script>
-<br><br><a href="https://circuits4you.com">Circuits4you.com</a>
-</body>
-</html>
-)=====";
+
+void handleLED() {
+    String ledState = "OFF";
+    String t_state = server.arg("LEDstate"); //Refer  xhttp.open("GET", "setLED?LEDstate="+led, true);
+    Serial.println(t_state);
+    if (t_state == "1")
+    {
+        digitalWrite(LED, LOW); //LED ON
+        ledState = "ON"; //Feedback parameter
+    }
+    else
+    {
+        digitalWrite(LED, HIGH); //LED OFF
+        ledState = "OFF"; //Feedback parameter
+    }
+
+    server.send(200, "text/plane", ledState); //Send web page
+}
+//==============================================================
+//                  SETUP
+//==============================================================
+void setup(void) {
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);     //Connect to your WiFi router
+    Serial.println("");
+
+    //Onboard LED port Direction output
+    pinMode(LED, OUTPUT);
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    //If connection successful show IP address in serial monitor
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+
+    server.on("/", handleRoot);      //Which routine to handle at root location. This is display page
+    server.on("/setLED", handleLED);
+    server.on("/readADC", handleADC);
+
+    server.begin();                  //Start server
+    Serial.println("HTTP server started");
+}
+//==============================================================
+//                     LOOP
+//==============================================================
+void loop(void) {
+    server.handleClient();          //Handle client requests
+}
