df3f598807327c57e2eec9ed935a9161f47d0167
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-01 13:09:56 +0900

    Create ESP8266_tempRead_AP02.ino

---
+++ Example/WebServer/IoT/ESP8266_tempRead_AP02/ESP8266_tempRead_AP02.ino
@@ -0,0 +1,52 @@
+// 온도 모니터링 웹서버(AP mode : 외부 인터넷 연결없이 Local WiFi Networking)
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+#include "index.h"
+
+//SSID and Password of your WiFi router
+const char* ssid = "ESPWebserver";
+const char* password = "12345678";
+
+ESP8266WebServer server(80);    // Server on port 80
+
+//===================================================================
+// IP(192.168.x.x)에 접속 후 열릴때 아래 루틴 실행
+//===================================================================
+void handleRoot() {
+    String s = MAIN_page;
+
+    int tempValue;
+    tempValue = 30;
+
+    // int 온도 값을 문자열로 변환 한 다음 HTML의 @@temp@@를 온도 값으로 바꾼다
+    s.replace("@@temp@@", String(tempValue));
+    server.send(200, "text/html", s);   // 브라우저에 웹 페이지 보낸다
+}
+//===================================================================
+//                    Power on setup
+//===================================================================
+void setup() {
+    Serial.begin(115200);
+    Serial.println("");
+    WiFi.mode(WIFI_AP);             // only Access Point
+    WiFi.softAP(ssid, password);    // Hotspot Start
+
+    IPAddress myIP = WiFi.softAPIP();   // Get IP address
+    Serial.print("HotSpot IP:");
+    Serial.println(myIP);
+
+    server.on("/", handleRoot);         // Which routine to handle at root location
+
+    server.begin(); //Start server
+    Serial.println("HTTP server started");
+}
+
+//===================================================================
+//                    Main Program Loop
+//===================================================================
+void loop() {
+    server.handleClient();
+}
