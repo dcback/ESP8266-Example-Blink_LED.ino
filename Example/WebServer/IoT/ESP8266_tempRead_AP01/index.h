ccff7b361b0c400bddbe1ce279b7c093ad5d5f57
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-01 12:33:38 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_tempRead_AP01/index.h
@@ -0,0 +1,20 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="3">
+    <title>Temperature Monitoring Web Server</title>
+</head>
+<body>
+    <center>
+        <h2>Temperature Monitoring Web Server</h2>
+        <div>
+            <h1>@@temp@@ &deg;C</h1>
+        </div>
+        <div>
+            <h2>Temperature</h2>
+        </div>
+    </center>
+</body>
+</html>
+)=====";
