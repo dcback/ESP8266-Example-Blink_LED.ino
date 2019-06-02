02fff0729f7d9a23b4760de20692c1c272e44004
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-02 18:28:04 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_tempRead_STA02/index.h
@@ -0,0 +1,22 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="3">
+    <title>Temperature Monitoring Web Server</title>
+</head>
+
+<body>
+    <center>
+        <h2 style="color:Blue;">Temperature Monitoring Web Server</h2>
+        <div>
+            <h1 style="color:Red;">@@temp@@ &deg;C</h1>
+        </div>
+        <div>
+            <h2 style="color:Green;">Temperature</h2>
+        </div>
+    </center>
+</body>
+</html>
+)=====";
