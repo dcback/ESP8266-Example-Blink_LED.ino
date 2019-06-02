bd7504dd91db0044c531c8cd7eb1562afa09a365
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-02 12:54:28 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_helloWorld_AP02/index.h
@@ -0,0 +1,12 @@
+ const char MAIN_page[] PROGMEM = R"=====(
+ <HTML>
+ <HEAD>
+    <TITLE>My Web Server page</TITLE>
+ </HEAD>
+ <BODY>
+    <CENTER>
+        <B>Hello world! esp8266 AP_mode</B>
+    </CENTER>
+ </BODY>
+ </HTML>
+ )=====";
