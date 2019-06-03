c50a6f29e16cdd8397f75a503592ed7a550fe875
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-03 19:54:57 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_analogRead_STA01/index.h
@@ -0,0 +1,32 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="3">
+    <title>Analog Monitoring Web Server</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+</head> 
+
+<body>
+    <h2>Analog Monitoring Web Server</h2>
+    <p>Analog Pin(A0) Voltage Reading table.</p>
+    <table style="width:100%" align="center">
+        <tr>
+            <th>Analog Pin</th>
+            <th>Reading Value</th>
+        </tr>
+        <tr align="center">
+            <td>A0 Pin</td>
+            <td>@@temp@@</td>
+        </tr>
+    </table>
+    <h3>Using the CSS border property</h3>
+</body>
+
+</html>
+)=====";
