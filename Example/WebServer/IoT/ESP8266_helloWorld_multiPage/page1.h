bbc292fcfd9f3aaf3e08c31d644f2d9ab4583493
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-06 09:49:53 +0900

    Update page1.h

--- Example/WebServer/IoT/ESP8266_helloWorld_multiPage/page1.h
+++ Example/WebServer/IoT/ESP8266_helloWorld_multiPage/page1.h
@@ -15,18 +15,15 @@ const char htmlPage1[] PROGMEM = R"=====(
         <h1>nodeMCU Web Server</h1>
         <p style="color:blue;">nodeMCU multiPage Web Server - Page1.</p>
         <p>
-            <a class="btn btn-primary disabled" href="/" role="button">nodeMCU site Link</a>
-            <a class="btn btn-success" href="/page2" role="button">nodeMCU webPage</a>
-            <a class="btn btn-danger" href="/page3" role="button">nodeMCU IoT WebServer</a>
+            <a class="btn btn-primary disabled" href="/" role="button">Site Link</a>
+            <a class="btn btn-success" href="/page2" role="button">WebPage</a>
+            <a class="btn btn-danger" href="/page3" role="button">IoT WebServer</a>
         </p>
         <ul>
             <li><a href="https://en.wikipedia.org/wiki/NodeMCU">nodeMCU WiKi</a></li>
-            <li><a
-                    href="https://www.electronicwings.com/public/images/user_images/images/NodeMCU/NodeMCU%20Basics%20using%20Arduino%20IDE/NodeMCU%20GPIO/NodeMCU%20GPIOs.png">nodeMCU
-                    Pinout</a></li>
-            <li><a
-                    href="https://c.76.my/Malaysia/arduino-iot-lolin-nodemcu-v3-lua-based-esp8266-wifi-development-board-littlecraft-1801-27-littlecraft@4.jpg">nodeMCU
-                    V2, V3</a></li>
+            <li><a href="https://www.electronicwings.com/public/images/user_images/images/NodeMCU/NodeMCU%20Basics%20using%20Arduino%20IDE/NodeMCU%20GPIO/NodeMCU%20GPIOs.png">nodeMCU Pinout</a></li>
+            <li><a href="https://c.76.my/Malaysia/arduino-iot-lolin-nodemcu-v3-lua-based-esp8266-wifi-development-board-littlecraft-1801-27-littlecraft@4.jpg">nodeMCU V2, V3</a></li>
+            <li><a href="http://kiet00.iptime.org/">IoT DEMO WebServer</a></li>
         </ul>
         <p>Designed by ORY (C) 2019 <a
                 href="https://github.com/dcback/ESP8266/tree/master/Example/WebServer/IoT/ESP8266_helloWorld_multiPage">GithubLink.</a>
