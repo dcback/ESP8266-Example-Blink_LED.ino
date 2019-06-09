3e7f4cb2408f799beb4ebcf7dbdeb20c36d3965d
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-09 12:12:49 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_ajax_STA01/index.h
@@ -0,0 +1,40 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+
+<body>
+    <center>
+        <div>
+            <h4>ESP8266 nodeMCU Update Webpage without refresh</h4>
+        </div>
+        <div style="color:gray;">
+            <b>ADC Value is : <span id="sen1Value" style="color:red">0</span></b><br>
+        </div>
+
+        <script>
+            function getData() {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("sen1Value").innerHTML =
+                            this.responseText;
+                    }
+                };
+                xhttp.open("GET", "readSen1", true);
+                xhttp.send();
+            }
+
+            setInterval(function () {
+                // Call a function repetatively with 2 Second interval
+                getData();
+            }, 1000); //1000mSeconds update rate
+        </script>
+
+        <h5>Designed by ORY (C) 2019 <a
+                href="https://github.com/dcback/ESP8266/tree/master/Example/WebServer/IoT/ESP8266_ajax_STA01">GithubLink.</a>
+        </h5>
+    </center>
+</body>
+
+</html>
+)=====";
