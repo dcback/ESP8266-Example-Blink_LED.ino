d1bf837304d502fba16f68df44ebef0ab9d0eb5e
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-10 21:06:34 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/ESP8266_RSSI01/index.h
@@ -0,0 +1,22 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="1">
+    <title>RSSI</title>
+</head>
+
+<body>
+    <center>
+        <h2>ESP8266 WiFi Signal Strength</h2>
+        <p> WiFi Signal Strength : <span style="color:red;font-weight:bold">@@value@@</span> dBm</p>
+        <h5>Designed by ORY (C) 2019 <a
+                href="https://github.com/dcback/ESP8266/tree/master/Example/WebServer/IoT/ESP8266_RSSI01">GithubLink.</a>
+        </h5>
+        <h5><a href="https://www.netspotapp.com/images/upload/screens/WF_S_S_4.jpg">RSSI</a>
+        </h5>
+    </center>
+</body>
+
+</html>
+)=====";
