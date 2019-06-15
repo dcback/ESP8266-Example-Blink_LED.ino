816a9ab527262815ee2e018d44e174a9369f2b50
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-15 21:32:09 +0900

    Update ledBlink01.ino

--- Example/WebServer/IoT/Basic/LED/ledBlink01.ino
+++ Example/WebServer/IoT/Basic/LED/ledBlink01.ino
@@ -3,13 +3,13 @@
 
 // 최초 1회 실행(Power On or RESET)
 void setup() {
-    pinMode(2, OUTPUT);
+    pinMode(2, OUTPUT);     // GPIO2핀을 출력으로 정의
 }
 
 // 무한 반복 실행(실질적 작동단계)
 void loop() {
     digitalWrite(2, HIGH);  // GPIO2(D4) 핀 출력을 HIGH
-    delay(5000);            // 5000ms(5초) 지연(유지)
+    delay(1000);            // 1000ms(5초) 지연(유지)
     digitalWrite(2, LOW);   // GPIO2(D4) 핀 출력을 LOW
-    delay(5000);            // 5000ms(5초) 지연(유지)
+    delay(1000);            // 1000ms(5초) 지연(유지)
 }
