9b04cc81abb72b8238d948daedf33fcf088de8d0
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-16 12:43:11 +0900

    Create ledBlink02.ino

---
+++ Example/WebServer/IoT/Basic/LED/ledBlink02.ino
@@ -0,0 +1,15 @@
+// nodeMCU LED Blink
+// GPIO2 == D4
+
+// 최초 1회 실행(Power On or RESET)
+void setup() {
+    pinMode(D4, OUTPUT);     // D4핀을 출력으로 정의
+}
+
+// 무한 반복 실행(실질적 작동단계)
+void loop() {
+    digitalWrite(D4, HIGH); // GPIO2(D4) 핀 출력을 HIGH
+    delay(100);             // 100ms(0.1초) 지연(유지)
+    digitalWrite(D4, LOW);  // GPIO2(D4) 핀 출력을 LOW
+    delay(100);             // 100ms(0.1초) 지연(유지)
+}
