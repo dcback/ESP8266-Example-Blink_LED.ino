15d126326480574c2ca25a8ecdb48c167eabfb7c
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-23 14:47:02 +0900

    Create gpioRead06.ino

---
+++ Example/WebServer/IoT/Basic/GPIO/gpioRead06.ino
@@ -0,0 +1,16 @@
+// PIR Sensor analogRead(A0) -> Serial Monitor
+
+int readVal_A0;     // readVal_A0 정수 변수로 정의
+
+void setup() {
+    Serial.begin(115200);    // 시리얼 출력(115200) 사용
+}
+
+void loop() {
+    readVal_A0 = analogRead(A0);        // A0 핀 읽기    
+    
+    // 시리얼 모니터 출력
+    Serial.print("A0 readValue:"); Serial.println(readVal_A0);
+    
+    delay(1000);    // 1초 지연
+}
