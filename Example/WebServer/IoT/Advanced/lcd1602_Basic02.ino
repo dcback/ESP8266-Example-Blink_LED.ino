ebb18ce2ab5adc46377436c6453dfe94b1f67dbf
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-25 10:04:26 +0900

    Create lcd1602_Basic02.ino

---
+++ Example/WebServer/IoT/Advanced/lcd1602_Basic02.ino
@@ -0,0 +1,31 @@
+/******************************************
+    I2C    Arduino  STM32F103  ESP8266
+    SDA      A4        PB7     D2(GPIO4)
+    SCL      A5        PB6     D1(GPIO5)
+*******************************************/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address(I2C) to 0x27
+
+void setup() {
+    lcd.begin();        // LCD initialize
+    lcd.backlight();    // Backlight on
+}
+
+void loop() {
+    int tempVal = random(10, 30);
+    int humiVal = random(50, 80);
+    
+    // LCD1602_I2C Display
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Temperature:"); lcd.print(tempVal);
+    lcd.print(" C"); 
+
+    lcd.setCursor(0, 1);
+    lcd.print("Humidity:"); lcd.print(humiVal); 
+    lcd.print(" %"); 
+    
+    delay(500);
+}
