76133471990ca9bf8f519152ca776e2358575c0d
DC Back <45322614+dcback@users.noreply.github.com>
2019-06-26 21:33:56 +0900

    Update tm1637_basic01.ino

--- Example/WebServer/IoT/Advanced/tm1637_basic01.ino
+++ Example/WebServer/IoT/Advanced/tm1637_basic01.ino
@@ -3,14 +3,14 @@
 #define CLK D3      // TM1637 pin assign
 #define DIO D4      // TM1637 pin assign
 
-TM1637Display dsp(CLK, DIO);
+TM1637Display display(CLK, DIO);
 
 void setup() {
-    dsp.setBrightness(7);       // min(0 ~ max(7)
+    display.setBrightness(7);  // min(0 ~ max(7)
 }
 
 void loop() {
     int numCnt = random(0, 9999);   // random number;
-    dsp.showNumberDec(numCnt);      // display numCnt value
+    display.showNumberDec(numCnt);      // display numCnt value
     delay(1000);
 }
