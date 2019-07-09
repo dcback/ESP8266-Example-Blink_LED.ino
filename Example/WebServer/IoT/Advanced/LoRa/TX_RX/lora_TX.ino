0c996e4672599a2148015e639f66b6fd84da35b3
DC Back <45322614+dcback@users.noreply.github.com>
2019-07-09 14:55:58 +0900

    Create lora_TX.ino

---
+++ Example/WebServer/IoT/Advanced/LoRa/TX_RX/lora_TX.ino
@@ -0,0 +1,54 @@
+// https://github.com/sandeepmistry/arduino-LoRa
+/***************************************************
+    [SPI]       ATm328p     STM32       ESP8266
+    MOSI        D11         PA7         D7(GPIO13)
+    MISO        D12         PA6         D6(GPIO12)
+    SCK/CLK     D13         PA5         D5(GPIO14)
+    NSS/CSN     D10         PA4         D8{GPIO15)
+    NRESET      D9          PC14        D0(GPIO16)
+    DIO0        D2          PA1         D1(GPIO5)
+    VCC         3.3V        3.3V        3.3V
+****************************************************/
+#define ledPin      D4
+#define csPin       D8
+#define resetPin    D0
+#define irqPin      D1
+
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+
+    pinMode(ledPin, OUTPUT);
+
+    LoRa.setPins(csPin, resetPin, irqPin);  // CS, RST, IRQ Pin Set
+
+    Serial.println("LoRa Sender");
+
+    if (!LoRa.begin(915E6)) {
+        Serial.println("Starting LoRa failed!");
+        while (1);
+    }
+}
+bool ledStatus = HIGH;
+void loop() {
+
+    Serial.print("Sending packet: ");
+    Serial.println(counter);
+
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("hello ");
+    LoRa.print(counter);
+    LoRa.endPacket();
+
+    counter++;
+
+    ledStatus = !ledStatus;
+    digitalWrite(ledPin, ledStatus);
+    delay(1000);
+}
