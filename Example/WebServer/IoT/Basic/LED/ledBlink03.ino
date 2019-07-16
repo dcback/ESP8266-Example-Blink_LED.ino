e0672542f3b72cc93251174ebdc9ef688c62cf71
DC Back <45322614+dcback@users.noreply.github.com>
2019-07-16 09:09:59 +0900

    Update ledBlink03.ino

--- Example/WebServer/IoT/Basic/LED/ledBlink03.ino
+++ Example/WebServer/IoT/Basic/LED/ledBlink03.ino
@@ -1,7 +1,7 @@
 // nodeMCU LED Blink
 // GPIO2 == D4
 
-int ledPin D4    // D4핀을 ledPin으로 정의(== #define ledPin D4)
+#define ledPin D4    // D4핀을 ledPin으로 정의
 
 void setup() {
     pinMode(ledPin, OUTPUT);    // D4핀을 출력으로 정의
