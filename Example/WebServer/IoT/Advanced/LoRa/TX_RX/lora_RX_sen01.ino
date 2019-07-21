f612b6d413440ff835f3b02d415717b4ee5f151b
DC Back <45322614+dcback@users.noreply.github.com>
2019-07-21 15:14:39 +0900

    Add files via upload

---
+++ Example/WebServer/IoT/Advanced/LoRa/TX_RX/lora_RX_sen01.ino
@@ -0,0 +1,45 @@
+#define ledPin      D4
+#define csPin       D8
+#define resetPin    D0
+
+#include <SPI.h>
+#include <LoRa.h>
+
+void setup() {
+   Serial.begin(115200);
+   while (!Serial);
+
+   pinMode(ledPin, OUTPUT);
+
+    LoRa.setPins(csPin, resetPin);  // CS, RST, (IRQ) Pin Set
+
+   Serial.println("LoRa Receiver");
+
+   if (!LoRa.begin(915E6)) {
+       Serial.println("Starting LoRa failed!");
+       while (1);
+   }
+}
+
+bool ledStatus = HIGH;
+
+void loop() {
+   // try to parse packet
+   int packetSize = LoRa.parsePacket();
+   if (packetSize) {
+       // received a packet
+       Serial.print("Received packet '");
+
+       // read packet
+       while (LoRa.available()) {
+           Serial.print((char)LoRa.read());
+       }
+
+       // print RSSI of packet
+       Serial.print("' with RSSI ");
+       Serial.println(LoRa.packetRssi());
+
+       ledStatus = !ledStatus;
+       digitalWrite(ledPin, ledStatus);
+   }
+}
