fa272df142ee2dfa3e9f5b759eb8f90524a83904
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-11 18:11:34 +0900

    Update ESP8266_helloWorld_AP02.ino

--- Example/WebServer/IoT/ESP8266_helloWorld_AP02/ESP8266_helloWorld_AP02.ino
+++ Example/WebServer/IoT/ESP8266_helloWorld_AP02/ESP8266_helloWorld_AP02.ino
@@ -2,41 +2,43 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-#include "index.h"  //HTML Webpage contents
-
-//SSID and Password of your WiFi router
+#include "index.h"              // web page
+ 
 const char *ssid = "ESPWebserver";
 const char *password = "12345678";
-
-ESP8266WebServer server(80); // Server on port 80
+ 
+ESP8266WebServer server(80);    // Server on port 80
 //=====================================================================
-// This rutine is exicuted when you open its IP in browser
+// This routine is executed when you open its IP in browser
 //=====================================================================
-void handleRoot() {
-    String s = MAIN_page;
-    server.send(200, "text/html", s);
+void handleRoot()
+{
+   String s = MAIN_page;
+   server.send(200, "text/html", s);
 }
 //=====================================================================
 //                              SETUP
 //=====================================================================
-void setup(void) {
-    Serial.begin(115200);
-    Serial.println("");
-    WiFi.mode(WIFI_AP);                 //only Access Point
-    WiFi.softAP(ssid, password);
-
-    IPAddress myIP = WiFi.softAPIP();   // Get IP address
-    Serial.print("HotSpot IP:");
-    Serial.println(myIP);
-
-    server.on("/", handleRoot); // Which routine to handle at root location
-
-    server.begin(); //Start server
-    Serial.println("HTTP server started");
+void setup(void)
+{
+   Serial.begin(115200);
+   Serial.println("");
+   WiFi.mode(WIFI_AP); // only Access Point
+   WiFi.softAP(ssid, password);
+ 
+   IPAddress myIP = WiFi.softAPIP(); // Get IP address
+   Serial.print("HotSpot IP:");
+   Serial.println(myIP);
+ 
+   server.on("/", handleRoot); // Which routine to handle at root location
+ 
+   server.begin(); // Start server
+   Serial.println("HTTP server started");
 }
-//===================================================================
+//=====================================================================
 //                    Main Program Loop
-//===================================================================
-void loop() {
-    server.handleClient();
+//=====================================================================
+void loop()
+{
+   server.handleClient();
 }
