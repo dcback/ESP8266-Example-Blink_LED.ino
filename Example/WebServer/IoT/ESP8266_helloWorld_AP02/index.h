501110b4562b501906a2d94d1f623ede39840f24
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-11 18:10:53 +0900

    Update index.h

--- Example/WebServer/IoT/ESP8266_helloWorld_AP02/index.h
+++ Example/WebServer/IoT/ESP8266_helloWorld_AP02/index.h
@@ -1,12 +1,11 @@
- const char MAIN_page[] PROGMEM = R"=====(
- <HTML>
- <HEAD>
+const char MAIN_page[] PROGMEM = R"=====(
+<HTML>
+<HEAD>
     <TITLE>My Web Server page</TITLE>
- </HEAD>
- <BODY>
-    <CENTER>
-        <B>Hello world! esp8266 AP_mode</B>
-    </CENTER>
- </BODY>
- </HTML>
- )=====";
+</HEAD>
+
+<BODY>
+    <CENTER> <B>Hello world! esp8266 AP_mode</B> </CENTER>
+</BODY>
+</HTML>
+)=====";
