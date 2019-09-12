3a9103b8a437b779a6f1e55cb7f25f3519a584aa
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-12 13:00:08 +0900

    Update ESP8266_tempRead_AP03.ino

--- Example/WebServer/IoT/ESP8266_tempRead_AP03/ESP8266_tempRead_AP03.ino
+++ Example/WebServer/IoT/ESP8266_tempRead_AP03/ESP8266_tempRead_AP03.ino
@@ -10,26 +10,30 @@ const char* password = "12345678";
 
 ESP8266WebServer server(80);    // Server on port 80
 
+int tempValue;                  // 온도 값 변수
 int cnt = 0;                    // 온도 증가용 변수
 //===================================================================
 // This routine is executed when you open its IP in browser
 //===================================================================
-void handleRoot() {
+void handleRoot()
+{
     String s = MAIN_page;
 
-    int tempValue;
     tempValue = 10 + cnt;
     cnt = cnt + 1;
-    if (cnt == 30) cnt = 0;
-    
+    if (tempValue == 20) cnt = 0;
+
     // int 온도 값을 문자열로 변환 한 다음 HTML의 @@temp@@를 온도 값으로 바꾼다
     s.replace("@@temp@@", String(tempValue));
     server.send(200, "text/html", s);   // 브라우저에 웹 페이지 보낸다
+
+    Serial.println(tempValue);          // 시리얼 모니터링
 }
 //===================================================================
 //                    Power on setup
 //===================================================================
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     Serial.println("");
     WiFi.mode(WIFI_AP);             // only Access Point
@@ -48,6 +52,7 @@ void setup() {
 //===================================================================
 //                    Main Program Loop
 //===================================================================
-void loop() {
+void loop()
+{
     server.handleClient();
 }
