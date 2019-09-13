1fde8d135f59e778e52f13e83ebed9b626c9cc38
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-14 07:59:37 +0900

    Update ESP8266_textSubmit_STA01.ino

--- Example/WebServer/IoT/ESP8266_textSubmit_STA01/ESP8266_textSubmit_STA01.ino
+++ Example/WebServer/IoT/ESP8266_textSubmit_STA01/ESP8266_textSubmit_STA01.ino
@@ -14,7 +14,8 @@ ESP8266WebServer server(80);        // Server on port 80
 //===================================================================
 // This routine is executed when you open its IP in browser
 //===================================================================
-void handleRoot() {
+void handleRoot() 
+{
     String s = MAIN_page; 
     
     // LED_state문자열을 HTML의 @@status@@ 변수로 바꾼다
@@ -24,42 +25,46 @@ void handleRoot() {
 //=====================================================================
 // This routine is executed when we press ON/OFF button i.e. form
 //=====================================================================
-void handleForm() {
-    conVal = server.arg("conValue");    //Get conValue
+void handleForm() 
+{
+    conVal = server.arg("conValue");    // Get conValue
     Serial.println(conVal);
     
-    server.sendHeader("Location", "/"); //Send web browser to root location
-    server.send(302, "text/html", "");  //Send browser 302 redirect go back to root location
+    server.sendHeader("Location", "/"); // Send web browser to root location
+    server.send(302, "text/html", "");  // Send browser 302 redirect go back to root location
 }
 //=================================================================
 //                    Power on setup
 //===================================================================
-void setup() {
+void setup() 
+{
     Serial.begin(115200);
-    WiFi.begin(ssid, password);    //Connect to your WiFi router
+    WiFi.begin(ssid, password);    // Connect to your WiFi router
 
     // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) 
+    {
         delay(500);
         Serial.print(".");
     }
 
-    //If connection successful show IP address in serial monitor
+    // If connection successful show IP address in serial monitor
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
-    Serial.println(WiFi.localIP()); //IP address assigned to your ESP
+    Serial.println(WiFi.localIP()); // IP address assigned to your ESP
 
-    server.on("/", handleRoot);     //Which routine to handle at root location
-    server.on("/form", handleForm); //These request sent when we click on button
+    server.on("/", handleRoot);     // Which routine to handle at root location
+    server.on("/form", handleForm); // These request sent when we click on button
 
-    server.begin(); //Start server
+    server.begin(); // Start server
     Serial.println("HTTP server started");
 }
 //===================================================================
 //                        Main Program Loop
 //===================================================================
-void loop() {
-    server.handleClient();     //handle client request
+void loop() 
+{
+    server.handleClient();     // handle client request
 }
