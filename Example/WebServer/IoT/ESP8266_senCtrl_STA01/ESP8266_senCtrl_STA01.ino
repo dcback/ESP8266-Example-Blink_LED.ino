2a68c69b695d6e6c87c189774f66d63f39b3d08d
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-14 08:33:52 +0900

    Update ESP8266_senCtrl_STA01.ino

--- Example/WebServer/IoT/ESP8266_senCtrl_STA01/ESP8266_senCtrl_STA01.ino
+++ Example/WebServer/IoT/ESP8266_senCtrl_STA01/ESP8266_senCtrl_STA01.ino
@@ -2,8 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-#include "index.h"      // web page
-
+#include "index.h"     // web page
 int Load1 = D1;
 int Load2 = D2;
 int Load3 = D3;
@@ -11,16 +10,19 @@ int Load4 = D4;         // Blue LED on Board
 
 String L1Status, L2Status, L3Status, L4Status, Temp;
 
-//SSID and Password of your WiFi router
+// SSID and Password of your WiFi router
 const char *ssid = "Backhomenet";
 const char *password = "1700note";
 
-ESP8266WebServer server(80);        // Server on port 80
+ESP8266WebServer server(80); // Server on port 80
 //===================================================================
 // This routine is executed when you open its IP in browser
 //===================================================================
-void handleRoot() {
+void handleRoot()
+{
     String s = MAIN_page;
+
+    Temp = 25;
 
     s.replace("@@L1@@", L1Status);
     s.replace("@@L2@@", L2Status);
@@ -28,83 +30,100 @@ void handleRoot() {
     s.replace("@@L4@@", L4Status);
     s.replace("@@TEMP@@", Temp);
 
-    server.send(200, "text/html", s);   // 브라우저에 웹 페이지를 보낸다
+    server.send(200, "text/html", s); // 브라우저에 웹 페이지를 보낸다
 }
 //=====================================================================
 // This routine is executed when we press ON/OFF button i.e. form
 //=====================================================================
-void handleForm() {
-    String L_state = server.arg("submit");  //Get submit value
+void handleForm()
+{
+    String L_state = server.arg("submit"); // Get submit value
 
     //Change Load-1 State as per request
-    if (L_state == "ON1") {
+    if (L_state == "ON1")
+    {
         L1Status = "ON";
-        digitalWrite(Load1, LOW);       //Load1 Turned on
+        digitalWrite(Load1, LOW);    // Load1 Turned on
     }
-    if (L_state == "OFF1") {
+    if (L_state == "OFF1")
+    {
         L1Status = "OFF";
-        digitalWrite(Load1, HIGH);      //Load1 Turned off
+        digitalWrite(Load1, HIGH);   // Load1 Turned off
     }
 
-    if (L_state == "ON2") {
+    if (L_state == "ON2")
+    {
         L2Status = "ON";
-        digitalWrite(Load1, LOW);       //Load1 Turned on
+        digitalWrite(Load2, LOW);    // Load2 Turned on
     }
-    if (L_state == "OFF2") {
+    if (L_state == "OFF2")
+    {
         L2Status = "OFF";
-        digitalWrite(Load1, HIGH);      //Load1 Turned off
+        digitalWrite(Load2, HIGH);   // Load2 Turned off
     }
 
-    if (L_state == "ON3") {
+    if (L_state == "ON3")
+    {
         L3Status = "ON";
-        digitalWrite(Load1, LOW);       //Load1 Turned on
+        digitalWrite(Load3, LOW);    // Load3 Turned on
     }
-    if (L_state == "OFF3") {
+    if (L_state == "OFF3")
+    {
         L3Status = "OFF";
-        digitalWrite(Load1, HIGH);      //Load1 Turned off
+        digitalWrite(Load3, HIGH);   // Load3 Turned off
     }
 
-    if (L_state == "ON4") {
+    if (L_state == "ON4")
+    {
         L4Status = "ON";
-        digitalWrite(Load1, LOW);       //Load1 Turned on
+        digitalWrite(Load4, LOW);    // Load4 Turned on
     }
-    if (L_state == "OFF4") {
+    if (L_state == "OFF4")
+    {
         L4Status = "OFF";
-        digitalWrite(Load1, HIGH);      //Load1 Turned off
+        digitalWrite(Load4, HIGH);   // Load4 Turned off
     }
 
     server.sendHeader("Location", "/"); //Send web browser to root location
-    server.send(302, "text/html", "");  //Send browser 302 redirect go back to root location
+    server.send(302, "text/html", "");  //Send browser 302 redirect go back
 }
 //=================================================================
 //                    Power on setup
 //===================================================================
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    WiFi.begin(ssid, password);    //Connect to your WiFi router
+    WiFi.begin(ssid, password); // Connect to your WiFi router
 
     // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
     }
 
-    //If connection successful show IP address in serial monitor
+    // If connection successful show IP address in serial monitor
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
-    Serial.println(WiFi.localIP()); //IP address assigned to your ESP
+    Serial.println(WiFi.localIP()); // IP address assigned to your ESP
 
-    server.on("/", handleRoot);     //Which routine to handle at root location
-    server.on("/form", handleForm); //These request sent when we click on button
+    server.on("/", handleRoot);     // Which routine to handle at root location
+    server.on("/form", handleForm); // These request sent when we click on button
 
-    server.begin(); //Start server
+    server.begin(); // Start server
     Serial.println("HTTP server started");
+
+    pinMode(Load1, OUTPUT);
+    pinMode(Load2, OUTPUT);
+    pinMode(Load3, OUTPUT);
+    pinMode(Load4, OUTPUT);
 }
 //===================================================================
 //                        Main Program Loop
 //===================================================================
-void loop() {
-    server.handleClient();     //handle client request
+void loop()
+{
+    server.handleClient(); // handle client request
 }
