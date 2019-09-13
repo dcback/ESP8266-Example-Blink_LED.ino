1822f2ac9d52ca158c903a54d4f02ded5e903217
DC Back <45322614+dcback@users.noreply.github.com>
2019-09-13 21:29:17 +0900

    Update ESP8266_ledControl_STA03.ino

--- Example/WebServer/IoT/ESP8266_ledControl_STA03/ESP8266_ledControl_STA03.ino
+++ Example/WebServer/IoT/ESP8266_ledControl_STA03/ESP8266_ledControl_STA03.ino
@@ -9,7 +9,7 @@
 String LED_state1 = "OFF";
 String LED_state2 = "OFF";
 
-//SSID and Password of your WiFi router
+// SSID and Password of your WiFi router
 const char *ssid = "Backhomenet";
 const char *password = "1700note";
 
@@ -17,7 +17,8 @@ ESP8266WebServer server(80);        // Server on port 80
 //===================================================================
 // This routine is executed when you open its IP in browser
 //===================================================================
-void handleRoot() {
+void handleRoot() 
+{
     String s = MAIN_page;
 
     // LED_state문자열을 HTML의 @@status@@ 변수로 바꾼다
@@ -28,57 +29,66 @@ void handleRoot() {
 //===================================================================
 // This routine is executed when we press ON/OFF button i.e. form
 //===================================================================
-void handleForm() {
-    String s1 = server.arg("button1");  //Get button1 state
-    if (s1 == "ON") {
-        digitalWrite(LED1, LOW);        //Turn LED1 on (LED is in reverse connection)
-        LED_state1 = "ON";              //Change status to ON
-    }
-    if (s1 == "OFF") {
-        digitalWrite(LED1, HIGH);       //Turn LED1 off (LED is in reverse connection)
-        LED_state1 = "OFF";             //Change status to OFF
+void handleForm() 
+{
+    String s1 = server.arg("button1");  // Get button1 state
+    if (s1 == "ON") 
+    {
+        digitalWrite(LED1, LOW);        // Turn LED1 on (LED is in reverse connection)
+        LED_state1 = "ON";              // Change status to ON
     }
     
-    String s2 = server.arg("button2");  //Get button2 state
-    if (s2 == "ON") {
-        digitalWrite(LED2, LOW);        //Turn LED2 on (LED is in reverse connection)
-        LED_state2 = "ON";              //Change status to ON
+    if (s1 == "OFF") 
+    {
+        digitalWrite(LED1, HIGH);       // Turn LED1 off (LED is in reverse connection)
+        LED_state1 = "OFF";             // Change status to OFF
     }
-    if (s2 == "OFF") {
-        digitalWrite(LED2, HIGH);       //Turn LED2 off (LED is in reverse connection)
-        LED_state2 = "OFF";             //Change status to OFF
+    
+    String s2 = server.arg("button2");  // Get button2 state
+    if (s2 == "ON") 
+    {
+        digitalWrite(LED2, LOW);        // Turn LED2 on (LED is in reverse connection)
+        LED_state2 = "ON";              // Change status to ON
     }
-    server.sendHeader("Location", "/"); //Send web browser to root location
-    server.send(302, "text/html", "");  //Send browser 302 redirect go back to root location
+    
+    if (s2 == "OFF") 
+    {
+        digitalWrite(LED2, HIGH);       // Turn LED2 off (LED is in reverse connection)
+        LED_state2 = "OFF";             // Change status to OFF
+    }
+    server.sendHeader("Location", "/"); // Send web browser to root location
+    server.send(302, "text/html", "");  // Send browser 302 redirect go back to root location
 }
 //===================================================================
 //                    Power on setup
 //===================================================================
-void setup() {
+void setup() 
+{
     Serial.begin(115200);
-    WiFi.begin(ssid, password);    //Connect to your WiFi router
+    WiFi.begin(ssid, password);    // Connect to your WiFi router
 
     // Wait for connection
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) 
+    {
         delay(500);
         Serial.print(".");
     }
 
-    //If connection successful show IP address in serial monitor
+    // If connection successful show IP address in serial monitor
     Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
-    Serial.println(WiFi.localIP()); //IP address assigned to your ESP
+    Serial.println(WiFi.localIP());     // IP address assigned to your ESP
 
     //Change Pin mode once we have sent the ESP IP address
     pinMode(LED1, OUTPUT);
     pinMode(LED2, OUTPUT);
-    digitalWrite(LED1, HIGH);        //LED1 Off State
-    digitalWrite(LED2, HIGH);        //LED2 Off State
+    digitalWrite(LED1, HIGH);           // LED1 Off State
+    digitalWrite(LED2, HIGH);           // LED2 Off State
 
-    server.on("/", handleRoot);         //Which routine to handle at root location
-    server.on("/form", handleForm);     //These request sent when we click on button
+    server.on("/", handleRoot);         // Which routine to handle at root location
+    server.on("/form", handleForm);     // These request sent when we click on button
 
     server.begin(); //Start server
     Serial.println("HTTP server started");
@@ -86,6 +96,7 @@ void setup() {
 //===================================================================
 //                        Main Program Loop
 //===================================================================
-void loop() {
-    server.handleClient();     //handle client request
+void loop() 
+{
+    server.handleClient();     // handle client request
 }
